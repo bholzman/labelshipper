77462192ccc0bd8b6ea6c31795f893403bc2d1b3
Ben Holzman <ben@hyperhyper.com>
2016-01-17 23:53:02 -0500

    warn on stock mismatch
    Signed-off-by: Ben Holzman <ben@hyperhyper.com>

--- models/accountreport.py
+++ models/accountreport.py
@@ -1,6 +1,96 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+EXPECTED_CD = {
+    "LS00001": 538,
+    "LS00002": 511,
+    "LS00003": 0,
+    "LS00004": 211,
+    "LS00005": 104,
+    "LS00006": 526,
+    "LS00007": 439,
+    "LS00009": 24,
+    "LS00010": 2,
+    "LS00011": 210,
+    "LS00012": 3,
+    "LS00013": 25,
+    "LS00014": 0,
+    "LS00015": 231,
+    "LS00017": 64,
+    "LS00018": 68,
+    "LS00019": 0,
+    "LS00020": 20,
+    "LS00021": 20,
+    "LS00022": 8,
+    "LS00023": 137,
+    "LS00024": 109,
+    "LS00025": 311,
+    "LS00026": 51,
+    "LS00027": 83,
+    "LS00028": 0,
+    "LS00029": 30,
+    "LS00030": 39,
+    "LS00031": 0,
+    "LS00032": 0,
+    "LS00033": 36,
+    "LS00034": 0,
+    "LS00035": 21,
+    "LS00036": 28,
+    "LS00037": 280,
+    "LS00038": 143,
+    "LS00039": 0,
+    "LS00040": 0,
+    "LS00041": 106,
+    "LS00042": 13,
+    "LS00043": 102,
+    "LS00044": 0,
+}
+
+EXPECTED_LP = {
+    "LS00001": 408,
+    "LS00002": 199,
+    "LS00003": 0,
+    "LS00004": 255,
+    "LS00005": 164,
+    "LS00006": 367,
+    "LS00007": 0,
+    "LS00009": 6,
+    "LS00010": 0,
+    "LS00011": 0,
+    "LS00012": 0,
+    "LS00013": 0,
+    "LS00014": 22,
+    "LS00015": 0,
+    "LS00017": 0,
+    "LS00018": 0,
+    "LS00019": 0,
+    "LS00020": 24,
+    "LS00021": 0,
+    "LS00022": 0,
+    "LS00023": 0,
+    "LS00024": 0,
+    "LS00025": 0,
+    "LS00026": 0,
+    "LS00027": 0,
+    "LS00028": 0,
+    "LS00029": 0,
+    "LS00030": 0,
+    "LS00031": 0,
+    "LS00032": 69,
+    "LS00033": 0,
+    "LS00034": 0,
+    "LS00035": 29,
+    "LS00036": 0,
+    "LS00037": 0,
+    "LS00038": 131,
+    "LS00039": 90,
+    "LS00040": 0,
+    "LS00041": 303,
+    "LS00042": 0,
+    "LS00043": 0,
+    "LS00044": 0,
+}
+
 class AccountReport(object):
     def __init__(self, account):
         self.__account = account
@@ -36,6 +126,10 @@ class AccountReport(object):
             digital += int(datum['Digital'] or 0)
             stockCD += int(datum['CD'] or 0)
             stockLP += int(datum['LP'] or 0)
+        if stockCD != EXPECTED_CD[self.__account.account]:
+            print "Stock mismatch: {} CD {} != {}".format(self.__account.account, stockCD, EXPECTED_CD[self.__account.account])
+        if stockLP != EXPECTED_LP[self.__account.account]:
+            print "Stock mismatch: {} LP {} != {}".format(self.__account.account, stockLP, EXPECTED_LP[self.__account.account])
         return """
 <tfoot>
     <tr>
